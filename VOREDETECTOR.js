8afd2aff4b62e175744c30b2888c4e17725698d7
Zelenyy <dangerdude119@gmail.com>
2021-01-04 19:31:08 +1100

    Added database for recording vorecount

--- VOREDETECTOR.js
+++ VOREDETECTOR.js
@@ -1,6 +1,8 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf } = require('telegraf');
+const MongoClient = require('mongodb').MongoClient;
 
 const key = require("./key.json");
+const url = key.DB_url;
 
 //Dont share this key
 const bot = new Telegraf(key.key);
@@ -8,11 +10,33 @@ const bot = new Telegraf(key.key);
 //try /start in the bot's dms
 bot.start((ctx) => ctx.reply('Welcome!'));
 
+bot.command('scoreboard', (ctx) => {
+    MongoClient(url).connect(async (err, db) => {
+        if (err) throw err;
+        var dbo = db.db(key.DB);
+        var top = await dbo.collection(`${ctx.message.chat.id}`).find().sort({vorecount:-1, _id:1}).limit(3);
+        var topcount = await top.count();
+
+
+        var boardstring = "Top vore posters! \n";
+        var rank = 1;
+        await top.forEach((o) => {
+            boardstring += `${rank}. ${o.name} has said 'vore' ${o.vorecount} times \n`
+            rank++
+        })
+
+        ctx.reply(boardstring);
+        
+        db.close();
+    });
+});
+
 //When it detects any text
 bot.on('text', (ctx) => {
     if (ctx.message.text.toLowerCase().replace(/ /g,'').trim().includes("vore")) {
         //look for vore here
-        console.log("Vore has been mentioned")
+        console.log(`${ctx.message.from.username} has sinned`)
+        
         if(global.voreTime < 0) {
             global.voreTime = new Date().getTime();
             ctx.reply('👀');
@@ -39,10 +63,34 @@ bot.on('text', (ctx) => {
             } 
             time += `${secs}.${pad(ms,3)} seconds`;
 
+
             //Print print print
-            ctx.reply(`it has been ${time} since someone has mentioned vore`);
+            ctx.reply(`${ctx.message.from.first_name} has sinned, it has been ${time} since someone has mentioned vore`);
             global.voreTime = new Date().getTime();
+
+            MongoClient.connect(url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db(key.DB)
+                var query = {"id" : `${ctx.message.from.id}`};
+                var updateobj = {};
+                dbo.collection(`${ctx.message.chat.id}`).findOne(query, (err, res) => {
+                    if (err) throw err;
+                    updateobj = res;
+                    if (updateobj == null) {
+                        updateobj = {id: `${ctx.message.from.id}`, name: `${ctx.message.from.first_name}`, vorecount: 0};
+                    }
+
+                    updateobj.vorecount++;
+                    
+                    dbo.collection(`${ctx.message.chat.id}`).updateOne(query, {$set: updateobj}, {upsert: true}, (err) => {
+                        if (err) throw err;
+                    });
+                    db.close();
+                });
+            });
+            
         }
+        
     }
 });
 
