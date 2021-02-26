f045e27c04c16e0066dda0879c282038650f5ec7
Zelenyy <dangerdude119@gmail.com>
2021-02-26 13:52:21 +1100

    Re-did code for bot.on message and added records

--- VOREDETECTOR.js
+++ VOREDETECTOR.js
@@ -34,10 +34,25 @@ bot.command(['scoreboard', 'voreboard'], (ctx) => {
     });
 });
 
+bot.command('record', (ctx) => {
+    MongoClient(url).connect(async (err, db) => {
+        if (err) throw err;
+        var dbo = db.db(key.DB);
+        var res = await dbo.collection(`${ctx.message.chat.id}`).findOne({"id": `${ctx.message.chat.id}`});
+
+        var time = formatTime(res.recordtime)
+
+        ctx.reply(`Current record for not saying vore for in this chat is: \n${time}`);
+
+        db.close();
+    });
+});
+
 
 //When it detects any text
-bot.on('message', (ctx) => {
+bot.on('message', async (ctx) => {
     var voreflag = false;
+
     if(ctx.message.text && ctx.message.text.toLowerCase().replace(/ /g, '').trim().includes("vore")) {
         voreflag = true;
     } 
@@ -49,75 +64,99 @@ bot.on('message', (ctx) => {
         //look for vore here
         console.log(`${ctx.message.from.username} has sinned`)
 
-        //database stuff
-        MongoClient.connect(url, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db(key.DB)
-            var query = {
-                "id": `${ctx.message.from.id}`
+        //Connect to DB
+        const db = await MongoClient.connect(url, {
+            useNewUrlParser: true,useUnifiedTopology: true
+        });
+
+        //set new vore time
+        var voreTime = Date.now();
+
+        var dbo = db.db(key.DB);
+
+        var user = await dbo.collection(`${ctx.message.chat.id}`).findOne({"id": `${ctx.message.from.id}`});
+        var localobj = await dbo.collection(`${ctx.message.chat.id}`).findOne({"id": `${ctx.message.chat.id}`});
+
+        if (user == null) {
+            user = {
+                id: `${ctx.message.from.id}`,
+                name: `${ctx.message.from.first_name}`,
+                vorecount: 0,
+                lastvoretime: voreTime,
+                currentmodifier: 1
             };
-            var updateobj = {};
-            dbo.collection(`${ctx.message.chat.id}`).findOne(query, (err, res) => {
-                if (err) throw err;
-                updateobj = res;
-                if (updateobj == null) {
-                    updateobj = {
-                        id: `${ctx.message.from.id}`,
-                        name: `${ctx.message.from.first_name}`,
-                        vorecount: 0,
-                        lastvoretime: global.voreTime,
-                        currentmodifier: 1
+        }
+
+        //Mute user, editing perms
+        var mutetime = (voreTime / 1000) + (60 * user.currentmodifier);
+
+        var extras = { 
+                        "permissions" : {
+                            "can_send_messages" : false // CHANGE TO FALSE 
+                        },
+                        "until_date" : mutetime
                     };
-                }
-
-                if(updateobj.lastvoretime != null || updateobj.currentmodifier != null) {
-                    if((Date.now() - updateobj.lastvoretime)/1000 < 300) {
-                        updateobj.currentmodifier++;
-                    } else {
-                        updateobj.currentmodifier = 1;
-                    }
-                } else {
-                    updateobj.lastvoretime = Date.now();
-                    updateobj.currentmodifier = 1;
-                }
-
-                //Mute user
-                var mutetime = (Date.now() / 1000) + (60 * updateobj.currentmodifier);
-                var extras = { 
-                                "permissions" : {
-                                    "can_send_messages" : false
-                                },
-                                "until_date" : mutetime
-                            };
-                console.log(mutetime);
-                ctx.restrictChatMember(ctx.message.from.id, extras)
-                    .catch((err) => {
-                        console.error(err);
-                    });
-                console.log(`User muted for ${updateobj.currentmodifier} minute (current modifier: ${updateobj.currentmodifier})`);
-                
-                if (global.voreTime < 0) {
-                    //First instance?
-                    global.voreTime = new Date().getTime();
-                    ctx.reply('👀');
-                } else {
-                    //Print print print
-                    var time = formatTime(new Date().getTime() - global.voreTime);
-                    ctx.reply(`${ctx.message.from.first_name} has sinned, it has been ${time} since someone has mentioned vore. They have been muted for ${updateobj.currentmodifier} minutes.`);
-                }
-
-                updateobj.lastvoretime = Date.now();
-                updateobj.vorecount++;
-
-                dbo.collection(`${ctx.message.chat.id}`).updateOne(query, {
-                    $set: updateobj
-                }, {
-                    upsert: true
-                }, (err) => {
-                    if (err) throw err;
-                });
-                db.close();
+                    
+        console.log(mutetime);
+        ctx.restrictChatMember(ctx.message.from.id, extras)
+            .catch((err) => {
+                console.error(`ERROR CODE: ${err.code}`);
             });
+        console.log(`User muted for ${user.currentmodifier} minute (current modifier: ${user.currentmodifier})`);
+
+        //Relevant updating
+        //If user had said vore within 5 mins of the last time, add 1 minute to mute
+        if(user.lastvoretime != null || user.currentmodifier != null) {
+            if((voreTime - user.lastvoretime)/1000 < 300) {
+                user.currentmodifier++;
+            } else {
+                user.currentmodifier = 1;
+            }
+        } else {
+            user.lastvoretime = voreTime;
+            user.currentmodifier = 1;
+        }
+
+        if (localobj == null) {
+            localobj = {
+                id: `${ctx.message.chat.id}`,
+                lastvoretime: voreTime,
+                recordtime: 0
+            };
+        }
+
+        //check for new record
+        var newrecordstr = "";
+        var newrecord = voreTime - localobj.lastvoretime;
+
+        console.log(`record time: ${localobj.recordtime}`);
+        console.log(`last vore time: ${localobj.lastvoretime}`);
+
+        if((newrecord) > localobj.recordtime) {
+            newrecordstr = "NEW RECORD!";
+            localobj.recordtime = newrecord;
+        }
+
+        //print print print
+        var time = formatTime(newrecord);
+        ctx.reply(`${newrecordstr} \n ${ctx.message.from.first_name} has sinned, it has been ${time} since someone has mentioned vore. They have been muted for ${user.currentmodifier} minutes.`);
+
+        localobj.lastvoretime = voreTime;
+
+        await dbo.collection(`${ctx.message.chat.id}`).updateOne({"id": `${ctx.message.chat.id}`}, {
+            $set: localobj
+        }, {
+            upsert: true
+        }, (err) => {
+            if (err) throw err;
+        });
+
+        await dbo.collection(`${ctx.message.chat.id}`).updateOne({"id": `${ctx.message.from.id}`}, {
+            $set: user
+        }, {
+            upsert: true
+        }, (err) => {
+            if (err) throw err;
         });
 
     }
@@ -127,7 +166,6 @@ bot.on('message', (ctx) => {
 //Start the bot with a little message
 bot.launch().then((o, e) => {
     console.log("Starting bot...");
-    global.voreTime = -1;
 });
 
 //Pad function for time element in the voretime
